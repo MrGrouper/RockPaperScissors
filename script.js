05becf69cd465068e4b133187571fdef3adf95d2
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2023-02-09 12:49:03 -0600

    add reset button feature, refactor rock paper scissors buttons

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// Initiate scores
+// // Initiate scores
 var playerScore = 0;
 var computerScore = 0;
 var round = 0;
 
+initiate()
+
+function initiate(){
+playerScore = 0;
+computerScore = 0;
+round = 0;
 document.getElementById("player_score").textContent = playerScore;
 document.getElementById("round").textContent = round;
 document.getElementById("computer_score").textContent = computerScore;
-
+document.getElementById("info"
+  ).textContent = "Can you beat the computer?"
+  return;
+}
 //Takes players click selection
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    game(button.id);
-    console.log(button.id);
-  });
-});
+var rockBtn = document.querySelector('#rock_btn');
+rockBtn.addEventListener("click", () => {
+    game('rock');
+  })
+  var paperBtn = document.querySelector('#paper_btn');
+  paperBtn.addEventListener("click", () => {
+      game('paper');
+    })
+   var scissorsBtn = document.querySelector('#scissors_btn');
+scissorsBtn.addEventListener("click", () => {
+    game('scissors');
+  })
+
+  //Reset button reset scores, display intro message
+const resetBtn = document.querySelector('#reset')
+resetBtn.addEventListener("click", () => {
+    initiate();
+  })
 
 //Generates random computer choice
 function getComputerChoice() {
@@ -27,10 +47,8 @@ function getComputerChoice() {
 function playRound(player) {
   // If player throw same thing its a tie
   var computer = getComputerChoice();
-  console.log(player);
-  console.log(computer);
   if (player === computer) {
-    alert("Tie! Shoot again");
+    document.getElementById("info").textContent = "Tie! Shoot again";
     return playerScore;
   }
   // Player loses
@@ -40,27 +58,55 @@ function playRound(player) {
     (player === "rock" && computer === "paper")
   ) {
     computerScore = computerScore + 1; // increment score
-    alert(`You lose! ${computer} beats ${player}.
-        You: ${playerScore} 
-        Computer: ${computerScore}`);
-    return playerScore;
+    if (computerScore === 5){ //if computer gets to 5 points exit to game function
+        game()
+        return;
+    }
+    else {
+    document.getElementById(
+      "info"
+    ).textContent = "You lose that hand!";
+    return computerScore;
+    }
   }
   //Player wins
   else {
     playerScore = playerScore + 1; //Increment Score
-    alert(`You Win! ${player} beats ${computer}.
-        You: ${playerScore}
-        Computer: ${computerScore}`);
-    return computerScore;
+    if (playerScore === 5){ //if player gets to 5 points exit to game function
+        game()
+        return;
+    }
+    else {
+    document.getElementById(
+      "info"
+    ).textContent = "You Win that hand!";
+    return playerScore;
+    }
   }
 }
 
 function game(player) {
-  round++;
-  playRound(player);
-  document.getElementById("player_score").textContent = playerScore;
-  document.getElementById("round").textContent = round;
-  document.getElementById("computer_score").textContent = computerScore;
+  if (computerScore === 5) {
+    document.getElementById("info").textContent = "You lose!";
+    return;
+  
+  } else if (playerScore === 5) {
+    document.getElementById("info").textContent = "You Win the round!";
+    return;
+  } else {
+    round++;
+    playRound(player);
+    document.getElementById("player_score").textContent = playerScore;
+    document.getElementById("round").textContent = round;
+    document.getElementById("computer_score").textContent = computerScore;
+  }
 }
 
-
+// function playAgain(){
+// playerScore === 0;
+// computerScore === 0;
+// round === 0;
+// document.getElementById("info"
+//   ).textContent = "Can you beat the computer?"
+//   return;
+// }
